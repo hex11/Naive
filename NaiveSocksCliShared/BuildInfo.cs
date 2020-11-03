f1980351833473e5984179ae07c930d9a15afbf4
hex11 <hekusu11@gmail.com>
2020-11-03 13:51:34 +0800

    Version 0.4.2.0

--- NaiveSocksCliShared/BuildInfo.cs
+++ NaiveSocksCliShared/BuildInfo.cs
@@ -13,7 +13,7 @@ namespace NaiveSocks
             }
         }
 
-        public const string Version = "0.4.1.2";
+        public const string Version = "0.4.2.0";
 
         public const bool Debug =
 #if DEBUG
