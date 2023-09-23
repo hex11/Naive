3ef50880d72822156fd5a5275d3603f8b630d4af
hex11 <hekusu11@gmail.com>
2023-09-23 13:23:13 +0800

    update UA header strings

--- NaiveSocks/NaiveSocksShared/Adapters/NaiveM/NaiveMOutAdapter.cs
+++ NaiveSocks/NaiveSocksShared/Adapters/NaiveM/NaiveMOutAdapter.cs
@@ -184,8 +184,9 @@ namespace NaiveSocks
         }
 
         static readonly string[] UAs = new[] {
-            "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:78.0) Gecko/20100101 Firefox/78.0",
-            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.96 Safari/537.36",
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:102.0) Gecko/20100101 Firefox/102.0",
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36",
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36 Edg/117.0.2045.36",
         };
 
         protected override void OnInit()
